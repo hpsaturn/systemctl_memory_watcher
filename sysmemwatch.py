8d98e8d2fc06bb97b484b0991078eff52b0cc1b7
Hpsaturn <hpsaturn@gmail.com>
2022-01-18 14:29:07 +0100

    updated header and help options

--- sysmemwatch.py
+++ sysmemwatch.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python
+# This file is part of the sysmemwatch:
+# https://github.com/hpsaturn/systemctl_memory_watcher
+# Copyright (c) 2022, @hpsaturn, Antonio Vanegas
+# https://hpsaturn.com, All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 """
 syswatchmem -- utility for see the memory of each scope launched with systemd-run
