7bb75d6e1e9a0d2427181c428c878bc9c234b759
Hpsaturn <hpsaturn@gmail.com>
2022-01-18 11:45:53 +0100

    first version working

--- syswatchmem.py
+++ syswatchmem.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 """
 syswatchmem -- utility for see the memory of each scope launched with systemd-run
 
@@ -18,13 +20,29 @@ Options:
 import time
 import tqdm
 from docopt import docopt
-import os
-
+import subprocess
 from tqdm import tqdm
 
+
+def printBar(scope, state, max):
+  try:
+    s = (100*int(state))/int(max)
+  except:
+    s = 0
+  for i in tqdm(range(100)):
+    # time.sleep(0.05)
+    if i == int(s):
+      break
 
 if __name__ == '__main__':
   arguments = docopt(__doc__, version='0.0.1')
   debugmode = arguments["--verbose"]
-  for i in tqdm(range(10)):
-    time.sleep(0.1)
+  # print(arguments)
+  for scope in arguments["<scope>"]:
+    print(scope)
+    memc="systemctl --user show "+scope+" | grep MemoryCurrent | sed 's/=/ /g' | awk '{print $2}'"
+    memx="systemctl --user show "+scope+" | grep MemoryMax | sed 's/=/ /g' | awk '{print $2}'"
+    mem_cur=subprocess.check_output(memc, shell=True, )
+    mem_max=subprocess.check_output(memx, shell=True)
+    printBar(scope, mem_cur, mem_max)
+
